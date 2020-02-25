0f1e32dff0ffb45fefd49465dd8045c2fb92b0e0
Wagner Truppel <trupwl@gmail.com>
2020-02-25 12:31:42 +0000

    Added platform and version specifiers

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Either",
+    platforms: [
+        .iOS(.v11),
+        .watchOS(.v5),
+        .tvOS(.v11),
+        .macOS(.v10_14)
+    ],
     products: [
         .library(
             name: "Either",
